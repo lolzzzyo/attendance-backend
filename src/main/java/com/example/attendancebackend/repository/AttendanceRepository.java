0c24f247b62a9c9e0cb2e77db4f74a8bb9c808c2
lolzzzyo <lolzzzyo@hotmail.com>
2026-09-22 19:13:30 +0200

    fix query

--- src/main/java/com/example/attendancebackend/repository/AttendanceRepository.java
+++ src/main/java/com/example/attendancebackend/repository/AttendanceRepository.java
@@ -4,6 +4,8 @@ import com.example.attendancebackend.model.Attendance;
 import com.example.attendancebackend.model.AttendanceStatus;
 import com.example.attendancebackend.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,5 +20,17 @@ public interface AttendanceRepository
 
     List<Attendance> findByEventId(Long eventId);
 
-    long countByEventIdAndStatusAndRole(Long eventId, AttendanceStatus status, Role role);
+    @Query("""
+    SELECT COUNT(a)
+    FROM Attendance a
+    JOIN a.account acc
+    WHERE a.event.id = :eventId
+      AND a.status = :status
+      AND acc.role = :role
+""")
+    long countByEventIdAndStatusAndRole(
+            @Param("eventId") Long eventId,
+            @Param("status") AttendanceStatus status,
+            @Param("role") Role role
+    );
 }
